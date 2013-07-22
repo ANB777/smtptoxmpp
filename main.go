5f7ad304e3952f1e18bf25f318889dcb43283255
Emery Hemingway <emery@fuzzlabs.org>
2013-07-22 12:32:05 -0500

    Seems to work

--- main.go
+++ main.go
@@ -1,139 +1,207 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"flag"
+	"fmt"
 	xmpp "github.com/3M3RY/go-xmpp"
+	"github.com/BurntSushi/toml"
+	"log"
+	"net"
 	"net/textproto"
 	"os"
+	"strings"
 )
 
-var (
-	childLimit        = flag.Int("n", 8, "limit concurrent processing routines to n")
-	hostname          = flag.String("hostname", "", "hostname to report to clients, defaults to $HOSTNAME")
-	twoTwentyGreeting string
-	twoFiftyReply     string
-)
+type smtpConfig struct {
+	Hostname string `toml:"hostname"`
+	Port     int16  `toml:"port"`
+}
+
+type xmppConfig struct {
+	Domain string `toml:"domain"`
+	Name   string `toml:"name"`
+	Secret string `toml:"secret"`
+	Server string `toml:"server"`
+	Port   int16  `toml:"port"`
+}
+
+type tomlConfig struct {
+	Smtp smtpConfig `toml:"smtp"`
+	Xmpp xmppConfig `toml:"xmpp"`
+}
 
 func isValid(recipient string) bool {
 	return true
 }
 
-func newXMPPClient() {
-
+func stripAddr(s string) (address string) {
+	address = strings.Split(s, "<")[1]
+	address = address[:strings.Index(address, ">")]
+	return
 }
 
-func process(conn *net.Conn) {
-	defer c.Close()
+func stripAddrs(s string) (addresses []string) {
+	addresses = strings.Split(s, "<")[1:]
+	for i, a := range addresses {
+		addresses[i] = a[:strings.Index(a, ">")]
+	}
+	return
+}
+
+func process(conn net.Conn) {
+	defer conn.Close()
 	w := textproto.NewWriter(bufio.NewWriter(conn))
 	err := w.PrintfLine(twoTwentyGreeting)
 	if err != nil {
-		fmt.Println(err)
+		log.Print("SMTP Error: ", err)
 		return
 	}
 
 	r := textproto.NewReader(bufio.NewReader(conn))
 	s, err := r.ReadLine()
+	if err != nil {
+		log.Print("SMTP Error: ", err)
+		return
+	}
+
+	log.Print("\t", conn.RemoteAddr(), "\t", s)
+
 	switch s[:4] {
 	case "EHLO":
 		w.PrintfLine(twoFiftyReply)
-
 	case "HELO":
-
+		w.PrintfLine(twoFiftyGreeting)
 	default:
+		log.Print("SMTP Error: client sent this shit: ", s)
 		return
 	}
 
-	s, err := r.ReadLine()
+	s, err = r.ReadLine()
 	if err != nil {
-		fmt.Println(err)
+		log.Print("SMTP Error: ", err)
 		return
 	}
+
 	if s[:10] != "MAIL FROM:" {
-		fmt.Println("Did not receive proper MAIL command")
+		log.Print("SMTP Error: client sent '", s, "' instead of MAIL FROM")
 		return
 	}
+	sender := stripAddr(s[10:])
+
 	w.PrintfLine("250 OK")
 
 	s, err = r.ReadLine()
 	if err != nil {
+		log.Print("SMTP Error: ", err)
 		fmt.Println(err)
 		return
 	}
 	// TODO may get mail for more than one recipient
 	if s[:8] != "RCPT TO:" {
-		fmt.Println("Did not recieve RCPT command")
+		log.Print("SMTP Error: client sent '", s, "' instead of RCPT TO")
 	}
 
-	rcpt := s[8:]
-	if !isValid(rcpt) {
-		fmt.Println("Ignoring mail for", rcpt)
-		return
-	}
+	recipients := stripAddrs(s[8:])
+	//if !isValid(rcpt) {
+	//	fmt.Println("Ignoring mail for", rcpt)
+	//	return
+	//}
+	w.PrintfLine("250 OK")
 
-	s, err := r.ReadLine()
+	s, err = r.ReadLine()
 	if err != nil {
-		fmt.Println(err)
+		log.Print("SMTP Error: ", err)
 		return
 	}
 	if s != "DATA" {
-		fmt.Println("Did not receive data command the way we like it")
+		log.Print("SMTP Error: expected DATA, got ", s)
 		return
 	}
-	w.PrintfLine("354")
+	w.PrintfLine("354 End data with <CR><LF>.<CR><LF>")
 
-	dr := textproto.NewDotReader(conn)
+	dr := r.DotReader()
 	buf := new(bytes.Buffer)
 	_, err = buf.ReadFrom(dr)
 	if err != nil {
-		fmt.Println("Error: err")
+		log.Print("SMTP Error: ", err)
 		return
 	}
 
-	// TODO just drop everything after the first >
-	rcpt = rcpt[1 : len(rcpt)-1]
+	msg := fmt.Sprint("Originally from mail:", sender, "\n\n", buf.String())
 
-	xmppMu.Lock()
-	defer xmppMu.Unlock()
-	if xmppClient == nil {
-		xmppClient, err := xmpp.NewClientNoTLS(domain, user, password)
+	for _, recipient := range recipients {
+		err = component.SendMessage("smtp.localhost", recipient, "mail:" + sender, msg)
 		if err != nil {
 			// TODO inform the client that recieving the message has failed
-			fmt.Println("Could not connect to XMPP server:", err)
-			return
+			log.Print("XMPP Error: failed to send message: ", err)
 		}
-	}
-	defer xmppClient.Close()
-
-	err = xmppClient.SendMessage(rcpt, "", buf.String())
-	if err != nil {
-		// TODO inform the client that recieving the message has failed
-		fmt.Println("Error sending XMPP message:", err)
 	}
 	w.PrintfLine("250 OK")
 }
 
+var (
+	hostname          = flag.String("hostname", "", "hostname to report to clients, defaults to $HOSTNAME")
+	twoTwentyGreeting string
+	twoFiftyGreeting  string
+	twoFiftyReply     string
+	component         *xmpp.Component
+	config            *tomlConfig
+)
+
 func main() {
 	flag.Parse()
 	if *hostname == "" {
-		&hostname = os.Getenv("HOSTNAME")
+		*hostname = os.Getenv("HOSTNAME")
 	}
-	twoTwentyGreeting = fmt.Sprintf("220 %s SMTP to XMPP gateway", *hostname)
-	twoFiftyReply = fmt.Sprintf("250-%s", *hostname)
-
-	l, err := net.Listen("tcp", ":25")
-	if err != nil {
-		fmt.Println("Could not listen on port 25:", err)
+	args := flag.Args()
+	if len(args) != 1 {
+		fmt.Println("USAGE:", os.Args[0], "CONFIG_FILE")
 		os.Exit(1)
 	}
-	defer l.CLose()
 
-	childChan := make(chan child, childLimit)
-	for i := 0; i < childLimit; i++ {
-		newChild(childChan)
+	_, err := toml.DecodeFile(args[0], &config)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+
+	if config.Smtp.Hostname == "" {
+		config.Smtp.Hostname, err = os.Hostname()
+		if err != nil {
+			log.Fatal("Error: could not determine hostname,", err)
+		}
+	}
+
+	twoTwentyGreeting = "220 " + config.Smtp.Hostname + " SMTP to XMPP gateway"
+	twoFiftyGreeting = "250 " + config.Smtp.Hostname
+	twoFiftyReply = "250-" + config.Smtp.Hostname
+
+	component, err = xmpp.NewComponent(config.Xmpp.Domain, config.Xmpp.Name, config.Xmpp.Secret, config.Xmpp.Server, config.Xmpp.Port)
+	if err != nil {
+		// TODO inform the client that recieving the message has failed
+		log.Fatal("XMPP Error: Could not connect to XMPP server,", err)
+	}
+	defer component.Close()
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", config.Smtp.Port))
+	if err != nil {
+		log.Fatal("SMTP Error: could not listen on port 25,", err)
+	}
+	defer l.Close()
+
+	// childChan := make(chan child, 8)
+	// for i := 0; i < 8; i++ {
+	// 	newChild(childChan)
+	// }
 
 	for {
 		conn, err := l.Accept()
+		if err != nil {
+			log.Print("SMTP Error: ", err)
+			continue
+		}
 		process(conn)
 
 		// 450  Requested mail action not taken: mailbox unavailable (e.g.,
